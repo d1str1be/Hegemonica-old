af7b18b28fabf60ec21eb13f247e62f7eba20464
d1str1be <bogdanrus@list.ru>
2020-11-09 22:34:33 +0500

    Небольшие правки
    Продвижение по изучению Android Studio и libGDX

--- core/src/com/chiposlavia/game/ChiposlaviaClass.java
+++ core/src/com/chiposlavia/game/ChiposlaviaClass.java
@@ -9,19 +9,24 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 public class ChiposlaviaClass extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
-	
+	float x = 0.0f;
+	float y = 0.0f;
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = new Texture("chipo.png");
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(img, x, y);
+		x++;
+		y++;
+
 		batch.end();
 	}
 	
