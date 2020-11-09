1a39c7c6d8d75ffeb180717562e913af5c62c701
d1str1be <bogdanrus@list.ru>
2020-11-09 14:49:07 +0500

    Обновление до libGDX
    Перенастройка проекта. Импорт проекта libGDX

---
+++ desktop/src/com/chiposlavia/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.chiposlavia.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.chiposlavia.game.ChiposlaviaClass;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ChiposlaviaClass(), config);
+	}
+}
