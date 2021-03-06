b18cd45ab1abf2436a88b5e59edd1360ef5d1d88
Bogdan Bescherepov <49095971+d1str1be@users.noreply.github.com>
2021-03-06 15:56:38 +0300

    Create PolygonCreator.java

---
+++ core/src/com/hegemonica/game/screens/playscreen/PolygonCreator.java
@@ -0,0 +1,110 @@
+package com.hegemonica.game.screens.playscreen;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.viewport.Viewport;
+
+import java.util.ArrayList;
+
+public class PolygonCreator implements Screen, InputProcessor {
+    ArrayList<Vector2> vertices = new ArrayList<Vector2>();
+    Vector2 touchPoint;
+    InputProcessor inputProcessor;
+    Viewport viewport;
+    boolean coordsAreSame;
+    ShapeRenderer shapeRenderer;
+
+    public PolygonCreator(Viewport viewport) {
+        this.viewport = viewport;
+        coordsAreSame = false;
+
+    }
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glLineWidth(2);
+
+    }
+
+    @Override
+    public void dispose() {
+        shapeRenderer.dispose();
+    }
+
+
+
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public boolean keyDown(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyTyped(char character) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDragged(int screenX, int screenY, int pointer) {
+        return false;
+    }
+
+    @Override
+    public boolean mouseMoved(int screenX, int screenY) {
+        return false;
+    }
+
+    @Override
+    public boolean scrolled(float amountX, float amountY) {
+        return false;
+    }
+
+
+
+}
