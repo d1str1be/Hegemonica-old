52c214bbf008b7ff616385f2319ae0dc092f2608
d1str1be <bogdanrus@list.ru>
2021-03-13 17:24:04 +0500

    sync 13.03.2021

--- core/src/com/hegemonica/game/logic/country/Province.java
+++ core/src/com/hegemonica/game/logic/country/Province.java
@@ -5,16 +5,32 @@ import com.hegemonica.game.logic.buildings.Building;
 import java.util.HashMap;
 
 public class Province {
-    public String name;
+    public class Climate{
+        public static final int ARCTIC = -3; // арктический
+        public static final int COLD = -2; // холодный
+        public static final int CHILL = -1;// прохладный
+        public static final int MODERATE = 0; // умеренный
+        public static final int WARM = 1;
+        public static final int HOT = 2;
+        public static final int SCORCHING = 3; //знойный
+        public static final int EQUATORIAL = 4; //экваториальный
+    }
+    public int climate;
+    public int landscape;
+    public int status;
 
     public int id;
+    public String name;
 
+<<<<<<< Updated upstream
     public double population;
     public double neededFood;
 
     public Country owner;
 
-    HashMap <Building, Integer> buidings;
+    HashMap <Building, Integer> buidings = new HashMap<>(){
+    };
+};
 
     public int maxBuildings(double population) {
         return (1 + (int)(Math.ceil(population) - 1) / 5);
@@ -22,12 +38,28 @@ public class Province {
 
     //статус провинции
     public class Status {
+=======
+    public Province(int climateType) {
+>>>>>>> Stashed changes
     }
-    //погода провинции
-    public class Climate {
-        final public String HOT = "Жаркий";
 
+    //статус провинции
+    public class Status {
     }
     //рельеф
     public class Landscape {}
+<<<<<<< Updated upstream
+=======
+    public void update(){
+        switch (climate){
+
+        }
+        switch (landscape){
+
+        }
+        switch (status){
+            
+        }
+    }
+>>>>>>> Stashed changes
 }
--- core/src/com/hegemonica/game/logic/scenarios/gemelch/Gemelch.java
+++ core/src/com/hegemonica/game/logic/scenarios/gemelch/Gemelch.java
@@ -4,11 +4,12 @@ import com.hegemonica.game.logic.Technology;
 import com.hegemonica.game.logic.country.Country;
 // ЭТО СЦЕНАРИЙ ГЕГЕМОНИИ МЕЛЬХИОРА. ЗДЕСЬ ЗАДАЕМ ВСЕ СТРАНЫ ЭТОГО СЦЕНАРИЯ, КАРТУ "ПАНГЕЯ" И ВСЕ ОСТАЛЬНОЕ ИЗ ЭТОЙ ИГРЫ
 public class Gemelch {
-    Country Tay =
+    public static final int COUNT_OF_RESOURCES = 26;
     public class Technologies{
         Technology music = new Technology(2.28, 1500){
             @Override
             public void onResearch(Country country) {
+                country.cash++;
             }
         };
     }
