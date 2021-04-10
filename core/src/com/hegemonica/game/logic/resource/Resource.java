015e78ca39536a58d02dcd43de254f6ff8fdb22f
Bogdan Bescherepov <bogdanrus@list.ru>
2021-04-10 14:17:43 +0300

    10.04.21 Resource Class Update
    It seems that`s final version of Resource class

--- core/src/com/hegemonica/game/logic/resource/Resource.java
+++ core/src/com/hegemonica/game/logic/resource/Resource.java
@@ -1,18 +1,26 @@
 package com.hegemonica.game.logic.resource;
 
+import com.hegemonica.game.logic.country.Country;
 import com.hegemonica.game.logic.country.Province;
 
 public class Resource {
     boolean canObtain;
     boolean isObtained;
     Province province;
+    int id;
 
-    public Resource() {
+    public static final int GOLD = 1;
+
+    public Resource(Province prov, int id) {
+        this.province = prov;
+        this.id = id;
         canObtain = false;
     }
+    public void update(){ // эффект от ресурса на следующий ход
+        switch(id){
+            // case GOLD:
+        }
+    }
 
-    public class Iron {}
-    public class Gems {}
-    public class Grain {}
 
 }
