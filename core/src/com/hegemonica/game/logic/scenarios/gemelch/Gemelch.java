f4701aacd6f84e6c37463ca1d12104103bf40189
Bogdan Bescherepov <bogdanrus@list.ru>
2021-04-17 14:39:36 +0300

    Update Gemelch.java

--- core/src/com/hegemonica/game/logic/scenarios/gemelch/Gemelch.java
+++ core/src/com/hegemonica/game/logic/scenarios/gemelch/Gemelch.java
@@ -4,7 +4,7 @@ import com.hegemonica.game.logic.Technology;
 import com.hegemonica.game.logic.country.Country;
 import com.hegemonica.game.logic.resource.Resource;
 
-// ЭТО СЦЕНАРИЙ. ЗДЕСЬ ЗАДАЕМ ВСЕ СТРАНЫ ЭТОГО СЦЕНАРИЯ, КАРТУ "ПАНГЕЯ" И ВСЕ ОСТАЛЬНОЕ ИЗ ЭТОЙ ИГРЫ
+// ЭТО СЦЕНАРИЙ. ЗДЕСЬ ЗАДАЕМ ВСЕ СТРАНЫ ЭТОГО СЦЕНАРИЯ, КАРТУ "ПАНГЕЯ" И ВСЕ ОСТАЛЬНОЕ
 public class Gemelch {
     public static final int COUNT_OF_RESOURCES = 26;
     public class Technologies{
