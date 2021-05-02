57cce0ef1e4df8331993a2fec00ced4770d52f5b
d1str1be <bogdanrus@list.ru>
2021-05-02 14:52:43 +0500

    Game icon update

--- desktop/src/com/hegemonica/game/desktop/DesktopLauncher.java
+++ desktop/src/com/hegemonica/game/desktop/DesktopLauncher.java
@@ -1,5 +1,6 @@
 package com.hegemonica.game.desktop;
 
+import com.badlogic.gdx.Files;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.hegemonica.game.Core;
@@ -7,6 +8,11 @@ import com.hegemonica.game.Core;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Hegemonica";
+		config.vSyncEnabled = false;
+		config.width = 1280;
+		config.height = 1024;
+		config.addIcon("icons/hegemonicalogo32x32.png", Files.FileType.Internal);
 		new LwjglApplication(new Core(), config);
 	}
 }
