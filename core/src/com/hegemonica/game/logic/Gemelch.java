e2efe0f71e2092bee105bc3389f7e2853e42289e
d1str1be <bogdanrus@list.ru>
2021-05-08 14:54:56 +0500

    New Labels

--- core/src/com/hegemonica/game/logic/Gemelch.java
+++ core/src/com/hegemonica/game/logic/Gemelch.java
@@ -14,56 +14,57 @@ public class Gemelch {
     public HUD hud;
     
     public GemelchGFX gfx;
-
+    
     public static int turnNumber;
     public int mapHeight;
     public int mapWidth;
-
-    public Country red;
-    public Country green;
-    public Country blue;
-    public Country yellow;
-    public Country nothing;
-
+    
+    public Country redCountry;
+    public Country greenCountry;
+    public Country blueCountry;
+    public Country yellowCountry;
+    public Country nothingCountry;
+    
     public Country turnCountry;
-
+    
     public int provCountWidth;
     public int provCountHeight;
     public int provincesCount;
     public Province[] provinces;
-
+    
     public Province leftBottomProv;
     public Province rightBottomProv;
     public Province leftTopProv;
     public Province rightTopProv;
-
-
+    
+    
     public Gemelch(Core game, int provCountWidth, int provCountHeight, Stage stage) {
         this.provCountWidth = provCountWidth;
         this.provCountHeight = provCountHeight;
-        hud = new HUD(game,this);
+        
         provincesCount = provCountWidth * provCountHeight;
         
         HegeLog.log(HegeLog.MAP, "prov in width = " + provCountWidth);
         HegeLog.log(HegeLog.MAP, "prov in height = " + provCountHeight);
-
+        
         gfx = new GemelchGFX(this);
         gfx.setStage(stage);
-
+        
         turnNumber = 1;
-        nothing = new Country(this, "Nothing", Country.ID.NOTHING, Color.WHITE);
-        red = new Country(this, "Red", Country.ID.RED, Color.RED);
-        green = new Country(this, "Green", Country.ID.GREEN, Color.GREEN);
-        blue = new Country(this, "Blue", Country.ID.BLUE, Color.BLUE);
-        yellow = new Country(this, "Yellow", Country.ID.YELLOW, Color.YELLOW);
-        turnCountry = red;
-
-
+        nothingCountry = new Country(this, "Nothing", Country.ID.NOTHING, Color.WHITE);
+        redCountry = new Country(this, "Red", Country.ID.RED, Color.RED);
+        greenCountry = new Country(this, "Green", Country.ID.GREEN, Color.GREEN);
+        blueCountry = new Country(this, "Blue", Country.ID.BLUE, Color.BLUE);
+        yellowCountry = new Country(this, "Yellow", Country.ID.YELLOW, Color.YELLOW);
+        turnCountry = redCountry;
+    
+        hud = new HUD(game, this);
+        
         provinces = new Province[provCountWidth * provCountHeight];
         for (int i = 0; i < provCountHeight; i++) {
             for (int j = 0; j < provCountWidth; j++) {
                 int iterator = i * provCountWidth + j;
-                provinces[iterator] = new Province(iterator, "Province " + iterator, nothing, new boolean[]{true}, false, 50 * j, 50 * i, 50, 50);
+                provinces[iterator] = new Province(iterator, "Province " + iterator, nothingCountry, new boolean[]{true}, false, 50 * j, 50 * i, 50, 50);
                 provinces[iterator].buildingInProcess = provinces[iterator].farm;
             }
         }
@@ -71,62 +72,62 @@ public class Gemelch {
         rightBottomProv = provinces[provCountWidth - 1];
         leftTopProv = provinces[(provCountHeight - 1) * provCountWidth];
         rightTopProv = provinces[provCountWidth * provCountHeight - 1];
-
-        leftBottomProv.owner = red;
+        
+        leftBottomProv.owner = redCountry;
         leftBottomProv.lProvName = new Label(leftBottomProv.name, leftBottomProv.defaultSkin, "red");
         leftBottomProv.lProvName.setSize(leftBottomProv.width * 0.2f, leftBottomProv.height * 0.2f);
         leftBottomProv.lProvName.setFontScale(0.5f);
         leftBottomProv.lProvName.setPosition(leftBottomProv.x + (leftBottomProv.width * 0.05f), leftBottomProv.y);
-
-        leftTopProv.owner = blue;
+        
+        leftTopProv.owner = blueCountry;
         leftTopProv.lProvName = new Label(leftTopProv.name, leftTopProv.defaultSkin, "blue");
         leftTopProv.lProvName.setFontScale(0.5f);
         leftTopProv.lProvName.setSize(leftTopProv.width * 0.2f, leftTopProv.height * 0.2f);
         leftTopProv.lProvName.setPosition(leftTopProv.x + (leftTopProv.width * 0.05f), leftTopProv.y);
-
-        rightTopProv.owner = yellow;
+        
+        rightTopProv.owner = yellowCountry;
         rightTopProv.lProvName = new Label(rightTopProv.name, rightTopProv.defaultSkin, "yellow");
         rightTopProv.lProvName.setFontScale(0.5f);
         rightTopProv.lProvName.setSize(rightTopProv.width * 0.2f, rightTopProv.height * 0.2f);
         rightTopProv.lProvName.setPosition(rightTopProv.x + (leftBottomProv.width * 0.05f), rightTopProv.y);
-
-        rightBottomProv.owner = green;
+        
+        rightBottomProv.owner = greenCountry;
         rightBottomProv.lProvName = new Label(rightBottomProv.name, rightBottomProv.defaultSkin, "green");
         rightBottomProv.lProvName.setFontScale(0.5f);
         rightBottomProv.lProvName.setSize(rightBottomProv.width * 0.2f, rightBottomProv.height * 0.2f);
         rightBottomProv.lProvName.setPosition(rightBottomProv.x + (rightBottomProv.width * 0.05f), rightBottomProv.y);
-
+        
         gfx.addProvincesToStage();
     }
-
+    
     public void onTurn() {
         switch (turnCountry.id) {
             case Country.ID.RED:
                 HegeLog.log("Gemelch", "Red turns");
-                red.onTurn();
-                turnCountry = green;
+                redCountry.onTurn();
+                turnCountry = greenCountry;
                 break;
             case Country.ID.GREEN:
                 HegeLog.log("Gemelch", "Green turns");
-                green.onTurn();
-                turnCountry = blue;
+                greenCountry.onTurn();
+                turnCountry = blueCountry;
                 break;
             case Country.ID.BLUE:
                 HegeLog.log("Gemelch", "Blue turns");
-                blue.onTurn();
-                turnCountry = yellow;
+                blueCountry.onTurn();
+                turnCountry = yellowCountry;
                 break;
             case Country.ID.YELLOW:
                 HegeLog.log("Gemelch", "Yellow turns");
-                yellow.onTurn();
-                turnCountry = red;
+                yellowCountry.onTurn();
+                turnCountry = redCountry;
                 turnNumber++;
+                hud.newTurn();
                 break;
         }
     }
     
-
-
+    
     public void render(OrthographicCamera camera, float delta) {
         gfx.render();
         for (Province x : provinces) {
@@ -134,7 +135,7 @@ public class Gemelch {
         }
         hud.render(delta);
     }
-
+    
     public float[] getProvX() {
         float[] provX = new float[provincesCount];
         for (int i = 0; i < provincesCount; i++) {
@@ -143,7 +144,7 @@ public class Gemelch {
         return provX;
         
     }
-
+    
     public float[] getProvY() {
         float[] provY = new float[provincesCount];
         for (int i = 0; i < provincesCount; i++) {
@@ -151,13 +152,13 @@ public class Gemelch {
         }
         return provY;
     }
-
+    
     /**
      * Returns polygon which contains tap point. Returns null if no province contains tap point
      *
      * @return Selected province
      */
-
+    
     public Province whichPolygonContainsPoint(float x, float y) {
         for (Province province : provinces) {
             if (province.contains(x, y))
@@ -165,10 +166,10 @@ public class Gemelch {
         }
         return null;
     }
-
+    
     public int[] getNeighborsIdList(Province province) {
         int id = province.id;
-
+        
         //угловые
         if (id == 0) {
             int[] neighbors = new int[4];
@@ -198,9 +199,9 @@ public class Gemelch {
             neighbors[2] = id - mapWidth;
             neighbors[3] = id - mapWidth - 1;
             return neighbors;
-
+            
             //крайние по сторонам
-
+            
         } else if (id < mapWidth) {
             int[] neighbors = new int[6];
             neighbors[0] = id;
@@ -237,9 +238,9 @@ public class Gemelch {
             neighbors[4] = id - mapWidth - 1;
             neighbors[5] = id + mapWidth - 1;
             return neighbors;
-
+            
             //все остальные
-
+            
         } else {
             int[] neighbors = new int[9];
             neighbors[0] = id;
@@ -254,7 +255,7 @@ public class Gemelch {
             return neighbors;
         }
     }
-
+    
     public boolean[] getBooleanNeighborsList(Province province) {
         int neighborsQuantity = getNeighborsIdList(province).length;
         int[] neighborsIntList = getNeighborsIdList(province);
